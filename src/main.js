e338c7a7ffdc3e4d99388afd03c1931da96e163a
zhanglei <zhang.lei.923@gmail.com>
2018-09-14 11:53:35 +0800

    up

--- src/main.js
+++ src/main.js
@@ -5,8 +5,28 @@ function doPostMessage(winObj, data, target, other){
     if(!winObj.postMessage){
         return;
     } 
-    delete data.messengerjs.isReq;
-    delete data.messengerjs.isResp;
-    delete data.messengerjs.from;
-    winObj.postMessage(data, target, other);
+    var messengerjs = data.messengerjs;
+    delete messengerjs.isReq;
+    delete messengerjs.isResp;
+    delete messengerjs.from;
+
+    messengerjs.isReq = undefined;
+    messengerjs.isResp = undefined;
+    messengerjs.from = undefined;
+
+    //messengerjs.aaaaa = 1111111;
+    messengerjs = JSON.parse(JSON.stringify(messengerjs))
+
+    var messengerjs2 = {}
+    for(var key in messengerjs){
+        if(key !== 'isReq' && key !== 'isResp' && key !== 'from'){
+            messengerjs2[key] = messengerjs[key];
+        }else{
+            
+        }
+    }
+    var data2 = {
+        messengerjs: messengerjs2
+    }
+    winObj.postMessage(data2, target, other);
 };
