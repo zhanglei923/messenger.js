4ec8c5afd559940636a522e8c36b3a223bd87d3c
zhanglei923 <zhang.lei.923@gmail.com>
2018-11-25 05:49:14 +0800

    up

---
+++ test.js
@@ -0,0 +1,23 @@
+let webpack = require('webpack')
+let pathutil = require('path')
+console.log(webpack)
+
+let config = {
+    mode: 'development', //production
+    devtool: 'eval-source-map',
+    module: {
+        // loaders: [
+        // ]
+    },
+    entry: {
+        'bundle': ['./src2/main.js'],
+    },
+    output: {
+        path: pathutil.resolve(__dirname, "./dist/"),
+        filename: '[name]2.js'
+    },
+    plugins: [
+    ]
+}
+let compiler = webpack(config);
+compiler.run((err, stats) => {});
