e26bd5be791a58158e60ffd0530aa9776dbd91cd
zhanglei <zhang.lei.923@gmail.com>
2018-11-28 03:49:42 +0800

    up

--- src2/listener.js
+++ src2/listener.js
@@ -1,4 +1,96 @@
-let listener={
+//listener
+let md5_util = require('blueimp-md5')
+import encrypt from './encrypt';
+let init = (messenger)=>{
+    var _listeningEvents = {}
+    var handleRequest = function (data) {
+        //console.log('on msg', window.location.href, data)
+        var data = data.data;
+        if(data && data.messengerjs /**&& data.messengerjs.isReq **/){
+            process(data.messengerjs)
+        }
+    }
+    var doResponse = (responseToken, result)=>{
+        //console.log('i-can-process-this-request:', eventName, args, responseToken, result)
+        var windows = messenger.getTargetWindows();
+        for(var i = 0; i < windows.length; i++){
+            var iframe = windows[i]
+            //console.log('send response:', eventName, responseToken, iframe)
+            var obj = {
+                messengerjs:{                          
+                    result
+                }
+            };
+            obj.messengerjs = encrypt.encryptMessageData(obj.messengerjs, {
+                isResp: true,
+                from: iframe.from,
+                responsePageId: messenger.getPageId(),
+                responseToken: encrypt.encodeStr(responseToken),
+            });
+            obj = JSON.parse(JSON.stringify(obj));
+            messenger.doPostMessage(iframe.win, obj, '*');
+        }
+    }
+    var process = function(data){
+        //invoke
+        var args = data.args;
+        var status = encrypt.decryptMessageData(data);   
+        var eventName = status.eventName;     
+        var responseToken = status.responseToken;
 
+        //console.warn('doResponse', responseToken, responseToken.length)
+        
+        var fn = _listeningEvents[eventName]
+        //console.log('process', !!fn, window.location.href)
+        if(fn && data){
+            if(status.isReq){
+                var result = fn.apply(window, args)
+                if(result && typeof result.then === 'function'){
+                    result.then((data)=>{
+                        doResponse(responseToken, data)
+                    })
+                }else{
+                    doResponse(responseToken, result)
+                }
+            }            
+        }
+        //转发
+        if(status.from === 'parent'){//继续向child传播
+            var iframes = document.getElementsByTagName('iframe');
+            for(var i = 0; i < iframes.length; i++){
+                status.from = 'parent';
+                //console.log('godeep', window.location.href, i, data)
+                var obj = {messengerjs:data}
+                obj = JSON.parse(JSON.stringify(obj));
+                messenger.doPostMessage(iframes[i].contentWindow, obj, '*');
+            }
+        }
+        if(status.from === 'child'){//继续向parent传播
+            //console.log('from child', window.location.href)
+            if(window !== window.parent){
+                status.from = 'child';
+                var obj = {messengerjs:data}
+                obj = JSON.parse(JSON.stringify(obj));
+                messenger.doPostMessage(window.parent, obj, '*');
+            }
+        }
+    }
+    messenger.listen = function(eventName, callback){
+        eventName = md5_util(eventName)
+        _listeningEvents[eventName] = callback;
+    };    
+    messenger.stopListen = function(eventName){
+        eventName = md5_util(eventName)
+        delete _listeningEvents[eventName];
+    };
+    if (window.addEventListener) {
+        window.addEventListener("message", handleRequest);
+    } else if (window.attachEvent) {
+        window.attachEvent("onmessage", handleRequest);
+    }
+    console.log('listening...')
+}
+let listener={
+    init
 }
 export default listener;
--- src2/requester.js
+++ src2/requester.js
@@ -1,4 +1,120 @@
-let requester={
+//request
+let md5_util = require('blueimp-md5')
+import encrypt from './encrypt';
+let init = (messenger)=>{
+    var generateToken = ()=>{
+        var time = (Math.random()*1.7+'') +'-'+ 2.3 * (new Date())
+        return md5_util(time.replace(/\./ig,''));
+    }
+    var _currentTarget = window.parent;
+    var _currentTargetHost = '*';
+    var _waitingPromiseMap = {};
+    var thisPageId = ''+generateToken();
 
+    messenger.getPageId= ()=>{
+            return thisPageId;
+        }
+    messenger.getTargetWindows= ()=>{
+            var iframelist = [];//[window.parent];
+            iframelist.push({
+                                is: 'self',
+                                from: 'self',//在对方看来，是self，parent，还是child
+                                win:window
+                            });
+            if(window !== window.parent) {
+                iframelist.push({
+                                    is: 'parent',
+                                    from: 'child',
+                                    win: window.parent
+                                });
+
+            }
+            var iframes = document.getElementsByTagName('iframe');
+            for(var i = 0; i < iframes.length; i++){
+                iframelist.push({
+                    is: 'child',
+                    from:'parent',
+                    win: iframes[i].contentWindow
+                })
+            }
+            return iframelist;
+        }
+        messenger.setTarget= function(target){
+            _currentTarget = target;
+            if(_currentTarget.contentWindow) _currentTarget = _currentTarget.contentWindow;
+            return messenger;
+        }
+        messenger.emit= function (){
+            var me = this;
+            var eventName = arguments[0];
+            eventName = md5_util(eventName)
+            var args = [];
+            for(var i = 1; i < arguments.length; i++){
+                args.push(arguments[i]);
+            }
+            var responseToken = ''+generateToken();
+            //console.warn('generateToken',responseToken.length)
+            var windows = me.getTargetWindows();
+            for(var i = 0; i < windows.length; i++){
+                var iframe = windows[i];
+                console.log('sent-req:', eventName, responseToken, iframe)
+                var obj = {
+                    messengerjs:{
+                        args,//本次请求的参数
+                    }
+                };
+                obj.messengerjs = encrypt.encryptMessageData(obj.messengerjs, {
+                    eventName,//请求的名字
+                    isReq: true,//表明是request
+                    from: iframe.from,                    
+                    requestPageId: thisPageId, //发起请求的页面id
+                    responseToken,//本次请求的token，一次性
+                });
+                obj = JSON.parse(JSON.stringify(obj));
+                messenger.doPostMessage(iframe.win, obj, _currentTargetHost);
+            }
+            _waitingPromiseMap[responseToken] = {
+                receiveCount: 0
+            };
+            return {
+                onResponse: (cb)=>{
+                    _waitingPromiseMap[responseToken].cb = cb;
+                }
+            };
+        }
+    
+    var handleResponse = (data)=>{
+        //console.log('on msg', window.location.href, data)
+        var data = data.data;
+        if(!data || !data.messengerjs)return;
+        var status = encrypt.decryptMessageData(data.messengerjs)
+        var responseToken = encrypt.decodeStr(status.responseToken)
+        //if(data.messengerjs && data.messengerjs.isResp && _waitingPromiseMap[responseToken]){
+        if(data.messengerjs && data.messengerjs.info){
+            if(status.isResp && _waitingPromiseMap[responseToken]){
+                process(responseToken, data.messengerjs)
+            }
+        }
+    };
+    var process = (responseToken, data)=>{
+        var result = data.result;
+        //console.warn('got response', window.location.href, result)
+        let stopReceive = ()=>{
+            delete _waitingPromiseMap[responseToken];
+        }
+        let count = ++_waitingPromiseMap[responseToken].receiveCount;
+        if(_waitingPromiseMap[responseToken].cb){
+            _waitingPromiseMap[responseToken].cb(result, {count: count, stopReceive: stopReceive});
+        }
+    };
+    if (window.addEventListener) {
+        window.addEventListener("message", handleResponse);
+    } else if (window.attachEvent) {
+        window.attachEvent("onmessage", handleResponse);
+    }
+    
+}
+let requester={
+    init
 }
 export default requester;
