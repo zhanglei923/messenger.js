b68daf154d1aeb6370c37c779754b9afe4058136
zhanglei <zhang.lei.923@gmail.com>
2018-11-28 12:07:41 +0800

    up

--- build2.js
+++ build2.js
@@ -1,4 +1,5 @@
 let webpack = require('webpack')
+let _ = require('lodash')
 let pathutil = require('path')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
@@ -28,9 +29,13 @@ compiler.run((err, stats) => {
     console.log('err', err)
 });
 
-let config_p = config;
-config_p.mode = 'production'
-config_p.output.filename = '[name].min.js'
+let config_p = {
+    mode: 'production',
+    output:{
+        filename: '[name].min.js'
+    }
+};
+config_p = _.extend(config, config_p)
 let compiler_p = webpack(config_p)
 compiler_p.run((err, stats) => {
     console.log('err_p', err)
